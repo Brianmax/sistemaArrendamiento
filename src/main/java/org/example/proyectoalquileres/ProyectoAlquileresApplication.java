93821b67e0c11a5d8feed416b4823ee8461a58dd
Brianmax <george.maxi@ucsp.edu.pe>
2024-02-26 18:41:25 -0500

    start

---
+++ src/main/java/org/example/proyectoalquileres/ProyectoAlquileresApplication.java
@@ -0,0 +1,12 @@
+package org.example.proyectoalquileres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAlquileresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoAlquileresApplication.class, args);
+    }
+}
---
+++ src/test/java/org/example/proyectoalquileres/ProyectoAlquileresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.proyectoalquileres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAlquileresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
